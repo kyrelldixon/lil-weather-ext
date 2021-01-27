f7bb4d1f3df13bb0a9617cb59b0b5b8fedf6ebe1
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-01-26 20:46:40 -0600

    chore(tailwind): Add all tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/popup/**/*.{svelte,html}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    ...colors,
   },
   variants: {
     extend: {},
