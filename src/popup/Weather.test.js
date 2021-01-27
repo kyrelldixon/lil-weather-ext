7e07fef91b05dfcf42ba2a7b5270eba3e39467c9
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-01-27 17:54:12 -0600

    test: Make sure Weather component is based on props

--- src/popup/Weather.test.js
+++ src/popup/Weather.test.js
@@ -3,7 +3,15 @@ import { render, screen } from "@testing-library/svelte";
 
 it("displays the temperature and description", () => {
   const props = { temperature: 46, description: "Mostly Sunny" };
-  render(Weather, props);
+  const { rerender } = render(Weather, props);
+
+  expect(screen.getByText(`${props.temperature} °`)).toBeInTheDocument;
+  expect(screen.getByText(props.description)).toBeInTheDocument;
+
+  props.temperature = 80;
+  props.description = "Cloudy";
+
+  rerender(props);
 
   expect(screen.getByText(`${props.temperature} °`)).toBeInTheDocument;
   expect(screen.getByText(props.description)).toBeInTheDocument;
