dca1eaa3bb7af32e681ad1047cec6e1d43ae89aa
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-01-27 14:41:05 -0600

    test: Make sure temp and description display

--- src/popup/Weather.test.js
+++ src/popup/Weather.test.js
@@ -1,8 +1,9 @@
 import Weather from "./Weather.svelte";
 import { render, screen } from "@testing-library/svelte";
 
-it("displays the temperature", () => {
+it("displays the temperature and description", () => {
   render(Weather);
 
-  expect(screen.getBy);
+  expect(screen.getByText(`46 °`)).toBeInTheDocument;
+  expect(screen.getByText(/mostly sunny/i)).toBeInTheDocument;
 });
