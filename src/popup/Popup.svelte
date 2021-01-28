7662320524378e45b8f0a77fcdd11ae6552314da
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-01-28 07:41:36 -0600

    feat(styles): Add popup padding and dark-mode transition

--- src/popup/Popup.svelte
+++ src/popup/Popup.svelte
@@ -2,6 +2,8 @@
   import Forecast from "./Forecast.svelte";
 </script>
 
-<main class="dark:bg-gray-900 dark:text-gray-50">
+<main
+  class="p-8 transition-colors duration-300 dark:bg-gray-900 dark:text-gray-50"
+>
   <Forecast />
 </main>
