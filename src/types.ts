405e7e1e95bc6c1f6262585cd4c18dc0daa8c868
Kyrell Dixon <kyrell@kyrelldixon.com>
2021-01-29 12:50:45 -0600

    chore(types): Add weather api response type

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,63 @@ export interface DailyForecast {
   description: string;
   datetime: Date;
 }
+
+export interface DailyForecastResponse {
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+  current: {
+    dt: number;
+    sunrise: number;
+    sunset: number;
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    uvi: number;
+    clouds: number;
+    visibility: number;
+    wind_speed: number;
+    wind_deg: number;
+    weather: [{ id: number; main: string; description: string; icon: string }];
+  };
+  daily: [
+    {
+      dt: number;
+      sunrise: number;
+      sunset: number;
+      temp: {
+        day: number;
+        min: number;
+        max: number;
+        night: number;
+        eve: number;
+        morn: number;
+      };
+      feels_like: {
+        day: number;
+        night: number;
+        eve: number;
+        morn: number;
+      };
+      pressure: number;
+      humidity: number;
+      dew_point: number;
+      wind_speed: number;
+      wind_deg: number;
+      weather: [
+        {
+          id: number;
+          main: string;
+          description: string;
+          icon: string;
+        }
+      ];
+      clouds: number;
+      pop: number;
+      uvi: number;
+    }
+  ];
+}
